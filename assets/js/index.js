f6b77c86e67d5134acc3a27558f36ea22a3044e3
dizars1776 <dizars1776@yahoo.gr>
2022-11-22 14:57:59 +0200

    add dynamically updating site's title based upon cake's current price

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,22 @@ window.onload = () => {
   // HIDE THE PHISHING WARNING CONTAINER
   const closeButton = document.getElementById("phishing-banner-close");
   closeButton.addEventListener("click", () => closeButton.parentNode.parentNode.style.display = "none");
+
+  // UPDATE THE SITE'S TITLE FOR PANCAKES CURRENT PRICE
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET","https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd");
+
+  xhr.onload = () => {
+      if (xhr.status == 200) {
+          const data =  JSON.parse(xhr.responseText);
+          document.title = "Home | PancakeSwap - " +"$" + data["pancakeswap-token"]["usd"]; 
+
+      }else{
+        document.title = "error";
+      }
+
+  };
+
+  xhr.send();
+  
 }; 
