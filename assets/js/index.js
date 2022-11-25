f66df6471813c0e0fc6d613cc63ecc490371ff43
dizars1776 <dizars1776@yahoo.gr>
2022-11-25 13:00:39 +0200

    feat: populate the navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,19 +5,20 @@ window.onload = () => {
 
   // UPDATE THE SITE'S TITLE FOR PANCAKES CURRENT PRICE
   let xhr = new XMLHttpRequest();
-  xhr.open("GET","https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd&precision=3");
+  xhr.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd&precision=3");
 
   xhr.onload = () => {
-      if (xhr.status == 200) {
-          const data =  JSON.parse(xhr.responseText);
-          document.title = "Home | PancakeSwap - " +"$" + data["pancakeswap-token"]["usd"]; 
+    if (xhr.status == 200) {
+      const data = JSON.parse(xhr.responseText);
+      const appendText = document.getElementById("cake-price");
+      // updates the site's title
+      document.title = "Home | PancakeSwap - " + "$" + data["pancakeswap-token"]["usd"];
+      // updates the cake price in navbar options section
+      appendText.innerHTML = appendText.innerHTML + data["pancakeswap-token"]["usd"];
 
-      }else{
-        document.title = "error";
-      }
-
+    } else {
+      document.title = "error";
+    }
   };
-
   xhr.send();
-  
 }; 
