f034da5c93de885d10afc682c95ef399b82966f7
dizars1776 <dizars1776@yahoo.gr>
2022-11-30 13:26:04 +0200

    refactor: linting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,31 @@
 window.onload = () => {
   // HIDE THE PHISHING WARNING CONTAINER
   const closeButton = document.getElementById("phishing-banner-close");
-  closeButton.addEventListener("click", () => closeButton.parentNode.parentNode.style.display = "none");
+  closeButton.addEventListener(
+    "click",
+    () => (closeButton.parentNode.parentNode.style.display = "none")
+  );
 
   // GET LATEST PRICE OF CAKE
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd&precision=3");
+  xhr.open(
+    "GET",
+    "https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd&precision=3"
+  );
 
   xhr.onload = () => {
     if (xhr.status == 200) {
       const data = JSON.parse(xhr.responseText);
       const appendText = document.getElementById("cake-price");
       // updates the site's title
-      document.title = "Home | PancakeSwap - " + "$" + data["pancakeswap-token"]["usd"];
+      document.title =
+        "Home | PancakeSwap - " + "$" + data["pancakeswap-token"]["usd"];
       // updates the cake price in navbar options section
-      appendText.innerHTML = appendText.innerHTML + data["pancakeswap-token"]["usd"];
-
+      appendText.innerHTML =
+        appendText.innerHTML + data["pancakeswap-token"]["usd"];
     } else {
       document.title = "error";
     }
   };
   xhr.send();
-}; 
+};
