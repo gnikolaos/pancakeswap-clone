03ce581d710ea2a4a1dc30838a62103a2aa2442e
gejjeg <johnson.godslove@gmail.com>
2023-01-11 09:14:42 +0100

    Footer Section

---
+++ assets/js/footer.js
@@ -0,0 +1,61 @@
+// function changeColor() {
+//     document.body.style.backgroundColor = 'white';
+// }
+
+// function changeColor() {
+//     const color = document.querySelector('body');
+//     color.c lassList.toggle('bgColor');
+// }
+
+
+function changeColor() {
+    let color = document.querySelector('body').classList.toggle('bgColor');
+    let color2 = document.getElementById('on-sm-screen').classList.toggle('bgColor');
+}
+
+
+window.onscroll = () => {
+    let goUp = document.getElementById('scrollUp');
+    if (document.documentElement.scrollTop > 400 || document.body.scrollTop > 400) {
+        goUp.style.display = "inline-flex";
+    } else {
+        goUp.style.display = "none";
+    }
+}
+
+// Window.onscroll = function() {scrollUp()};
+
+// function scrollUp() {
+//     if (document.body.scrollTop > 200 || document.Element.scrollTop > 200) {
+//         scrollUp.style.display = 'inlineflex'
+//     } else {
+//         scrollUp.style.display = 'none';
+//     }
+// }
+
+// window.onscroll = function() {scrollUp()};
+// function top() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+
+// Get the button:
+// let scroll = document.getElementById("scrollUp");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = scrollFunction();
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// When the user clicks on the button, scroll to the top of the document
+function toPageTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
