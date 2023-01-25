b43283d820344c770ad91f0056d4889de03faccf
unknown <Gejjeg>
2023-01-24 16:25:10 -0800

    feat: Added toggle color for button text and button svg

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,61 +1,29 @@
-// function changeColor() {
-//     document.body.style.backgroundColor = 'white';
-// }
-
-// function changeColor() {
-//     const color = document.querySelector('body');
-//     color.c lassList.toggle('bgColor');
-// }
-
-
-function changeColor() {
-    let color = document.querySelector('body').classList.toggle('bgColor');
-    let color2 = document.getElementById('on-sm-screen').classList.toggle('bgColor');
-}
-
-
-window.onscroll = () => {
-    let goUp = document.getElementById('scrollUp');
-    if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
-        goUp.style.display = "inline-flex";
-    } else {
-        goUp.style.display = "none";
+    // toggle dark mode for the page, buttons text color, and button svg color
+    function changeColor() {
+        let color = document.querySelector('body').classList.toggle('bgColor');
+        let color2 = document.getElementById('on-sm-screen').classList.toggle('bgColor');
+        document.getElementById('warningNo').classList.toggle('active-sun');
+        document.getElementById('likeMoon').classList.toggle('disabled-moon');
+        document.getElementsByClassName('btn-color')[0].classList.toggle('btn-color-change');
+        document.getElementsByClassName('btn-color')[1].classList.toggle('btn-color-change');
+        document.getElementsByClassName('btn-color')[2].classList.toggle('btn-color-change');
+        document.getElementsByClassName('btn-color')[3].classList.toggle('btn-color-change');
+    
+        console.log(document.getElementById("warningNo").classList);
     }
-}
+    
+    // Hiding the scroll to page top button at 200px away from the navbar 
+    window.onscroll = () => {
+        let goUp = document.getElementById('scrollUp');
+        if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
+            goUp.style.display = "inline-flex";
+        } else {
+            goUp.style.display = "none";
+        }
+    }
 
-// Window.onscroll = function() {scrollUp()};
-
-// function scrollUp() {
-//     if (document.body.scrollTop > 200 || document.Element.scrollTop > 200) {
-//         scrollUp.style.display = 'inlineflex'
-//     } else {
-//         scrollUp.style.display = 'none';
-//     }
-// }
-
-// window.onscroll = function() {scrollUp()};
-// function top() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
-
-
-// Get the button:
-// let scroll = document.getElementById("scrollUp");
-
-// When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = scrollFunction();
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// When the user clicks on the button, scroll to the top of the document
-function toPageTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+    // function to return to top of page when click
+    function toPageTop() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
